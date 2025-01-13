fc83d5aa00afa65e1ee2d08d2465d242ebc0919a
Chen Tang <36949272+DawnLightRise@users.noreply.github.com>
2025-01-13 02:18:08 -0600

    Update Solution_Q236.py

--- Solutions/Solution_Q236.py
+++ Solutions/Solution_Q236.py
@@ -1,4 +1,4 @@
-# Solution Provided by Chen Tang (2025)
+# Solution Provided by Chen Tang (January, 2025)
 # LeetCode Question: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
 
 # Definition for a binary tree node.
